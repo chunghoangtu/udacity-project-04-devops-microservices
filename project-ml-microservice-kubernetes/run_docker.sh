0e8013ea1aeffee8b0706433dbff731178124c22
DESKTOP-CNDF47Q\chunghoangtu <chunghoangtu@gmail.com>
2023-06-17 21:16:45 +0700

    Task 2 - Run a Container & Make a Prediction

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
+docker login
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --platform=linux/amd64 --tag=tuchung/udacity-prj4-kubernetes .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 tuchung/udacity-prj4-kubernetes:latest
