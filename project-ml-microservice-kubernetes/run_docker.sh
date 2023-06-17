785bde345f540da874475713853d2240df1e4883
DESKTOP-CNDF47Q\chunghoangtu <chunghoangtu@gmail.com>
2023-06-17 22:53:31 +0700

    Task 2/3 - Change docker image tag

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -5,7 +5,7 @@ docker login
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --platform=linux/amd64 --tag=tuchung/udacity-prj4-kubernetes .
+docker build --platform=linux/amd64 --tag=chunghoangtu/udacity-prj4-kubernetes .
 
 # Step 2: 
 # List docker images
@@ -13,4 +13,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 tuchung/udacity-prj4-kubernetes:latest
+docker run -p 8000:80 chunghoangtu/udacity-prj4-kubernetes:latest
