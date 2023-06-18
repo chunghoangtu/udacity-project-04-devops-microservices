1c259c1f5a272097ba5fd0591c5be919447e63d8
DESKTOP-CNDF47Q\chunghoangtu <chunghoangtu@gmail.com>
2023-06-18 11:34:35 +0700

    Task 5 /6 - Config, Deploy with Kubernetes and save log

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=chunghoangtu/udacity-prj4-kubernetes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity-prj4 --image=$dockerpath:latest --port 80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+sleep 60s
+kubectl port-forward pod/udacity-prj4 8000:80
 
